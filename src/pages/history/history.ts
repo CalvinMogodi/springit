7967629715642c70767717e450f333b103de54df
Calvin <cmogodi@gmail.com>
2018-11-16 23:31:12 +0200

    last

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -3,7 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AvaliableservieproviderPage } from '../avaliableservieprovider/avaliableservieprovider';
 import { Storage } from '@ionic/storage';
-import { Observable } from 'rxjs';
 
 /**
  * Generated class for the HistoryPage page.
@@ -18,13 +17,21 @@ import { Observable } from 'rxjs';
   templateUrl: 'history.html',
 })
 export class HistoryPage {
-  requests: Observable<any[]>;
+  requests = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase, private storage: Storage) {
     storage.get('userId').then((val) => {
       val = "eSSHR8AT00aNwZFgZTJWoLjd6Uo1";
       if(val){
-        this.requests = db.list('requests').valueChanges(); //.orderByChild('userId').equalTo(val).on();
+        this.db.database.ref().child('requests').orderByChild('userId').equalTo(val).on('value', snapshot => {
+          var result = snapshot.val();
+          if (result != null) {
+            snapshot.forEach(snap => {
+              var user = snap.val();     
+              this.requests.push(user);             
+            });       
+          }
+        });
       }
     });
    
